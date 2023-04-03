849215545450535a71de88256f72e66b902f698e
Samuel Prado <samuel.prado@meep-app.local>
2023-04-03 05:01:03 -0300

    implementando o UTF-8 e o método GetString() para buscar os dados formatados

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,42 @@
 ﻿using ByteBankIO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytes = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        while(numeroDeBytes != 0)
+        {
+            numeroDeBytes = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+        //public override int Read(byte[] array, int offset, int count);
 
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+
+        /*
+        foreach(var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
+        */
     }
 }
